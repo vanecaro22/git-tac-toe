029eab23261b59577001ef3fd121eea621f6535f
Melanie Burger <melanie.burger@rangle.io>
2019-01-04 16:25:06 -0500

    ❌ Places x move

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -1,6 +1,6 @@
 export const x = "❌";
 export const o = "⭕️";
 
-const moves = [[[], [], []], [[], [], []], [[], [], []]];
+const moves = [[[], [], []], [[], [x], []], [[], [], []]];
 
 export default moves;
