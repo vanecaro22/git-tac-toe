9e93c957636e0dcfd372fc0675305bc59ee22d7f
qiuqili <835671347@qq.com>
2019-05-07 18:50:03 -0400

    Rachel move 4

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -1,10 +1,6 @@
 export const x = "❌";
 export const o = "⭕️";
 
-const moves = [
-  [[o], [x], []],
-  [[], [], []],
-  [[o], [], []]
-];
+const moves = [[[o], [x], []], [[], [x], []], [[o], [], []]];
 
 export default moves;
