90f6ae14b5eac15e58c212f9a72041a2f297778b
Vanessa Mendoza <vanessamendoza@Vanessas-MacBook-Pro.local>
2019-05-07 18:53:33 -0400

    Vanessa: move 7

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -1,6 +1,9 @@
 export const x = "❌";
 export const o = "⭕️";
 
-const moves = [[[o], [x], []], [[], [x], []], [[o], [o], [x]]];
+const moves = [
+  [[o], [x], []],
+  [[o], [x], []], 
+  [[o], [o], [x]]];
 
 export default moves;
